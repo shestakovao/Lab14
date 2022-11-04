a988d214cf21da4c0996c8c8855f8cd69de4ff59
Алексей Шестаков <shestakov_ao@bk.ru>
2022-11-04 13:40:44 +0700

    Добавьте файлы проекта.

---
+++ Task1/Animal.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("{0}:",Name);
+            Say();
+        }
+    }
+
+
+    class Dog : Animal
+    {
+        string name;
+        string breedOfDog;
+
+        public override string Name
+        {
+            get {return name;}
+            set {name = value;}
+        }
+
+        public string BreedOfDog
+        {
+            get { return breedOfDog; }
+            set { breedOfDog = value; }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Я собака породы {0}, Гав!", BreedOfDog);
+        }
+
+        public Dog (string name, string breedOfDog)
+            :base (name)
+        {
+            this.Name = name;
+            this.BreedOfDog = breedOfDog;
+        }
+    }
+
+    class Cat : Animal
+    {
+        string name;
+        string presenceOfWool;
+
+        public override string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string PresenceOfWool
+        {
+            get { return presenceOfWool; }
+            set { presenceOfWool = value; }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Я {0} кошка, Мяу!", PresenceOfWool);
+        }
+
+        public Cat(string name, string presenceOfWool)
+            : base(name)
+        {
+            this.Name = name;
+            this.PresenceOfWool = presenceOfWool;
+        }
+    }
+
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Тима", "овчарка");
+            Cat cat = new Cat("Мурка", "лысая");
+            dog.ShowInfo();
+            cat.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
